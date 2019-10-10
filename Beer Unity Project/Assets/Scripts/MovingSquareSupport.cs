9c71ced855f819a5fede46bfb20b3f468dcf6ea5
RosaletJohn <55261812+RosaletJohn@users.noreply.github.com>
2019-10-10 18:03:03 -0500

    More Coding for Movement

--- Beer Unity Project/Assets/Scripts/MovingSquareSupport.cs
+++ Beer Unity Project/Assets/Scripts/MovingSquareSupport.cs
@@ -6,17 +6,17 @@ public class MovingSquareSupport : MonoBehaviour
 
 {
     public float moveSpeed = 1f;
-
+    private float verticalInput;
     private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.gravityScale = 20;
     }
     //Used Fixed Update for Physics code because it will mess in Update.
     private void FixedUpdate()
     {
-        
+        verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(0, verticalInput * moveSpeed);
     }
 }
