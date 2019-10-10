25c06e70a77eba9e943b07a3004a2c2084b44330
RosaletJohn <55261812+RosaletJohn@users.noreply.github.com>
2019-10-10 17:15:52 -0500

    Rigidbody
    Coded Rigidbody2D

--- Beer Unity Project/Assets/Scripts/Movement.cs
+++ Beer Unity Project/Assets/Scripts/Movement.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MovingSquareSupport : MonoBehaviour
 
 {
-    private float moveSpeed = 0.2f;
+    public float moveSpeed = 1f;
+
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-      
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.gravityScale = 20;
     }
-
+    //Used Fixed Update for Physics code because it will mess in Update.
     private void FixedUpdate()
     {
         
