f08186d79171fbf56a8c17748c73d8250aeff5c0
RosaletJohn <55261812+RosaletJohn@users.noreply.github.com>
2019-10-10 16:05:50 -0500

    First Code

---
+++ Beer Unity Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+{
+    private float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            //if player presses up arrow, we move the square up
+            if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, moveSpeed, 0);
+        
+        }
+            else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -moveSpeed, 0);
+        }
+    }
+}
